39093f2fe6973518dbd4279847501b131fec4967
saka1029 <saka1029@gmail.com>
2025-09-16 21:02:16 +0900

    70

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,5 +1,19 @@
 fn main() {
-    loop {
-        println!("Hello, world!");
+    let mut count = 0;
+    'countingup: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+        loop {
+            println!("rmaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'countingup;
+            }
+            remaining -= 1;
+        }
+        count += 1;
     }
+                println!("End counr = {}", count);
 }
