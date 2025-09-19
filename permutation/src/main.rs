b372fb3911720d5adfd46363d93ba6865994a866
saka1029 <saka1029@gmail.com>
2025-09-19 14:45:30 +0900

    permutation

---
+++ permutation/src/main.rs
@@ -0,0 +1,27 @@
+fn solve(n: usize, k: usize, i: usize, result: &mut Vec<usize>, used: &mut Vec<bool>) {
+//    println!("n={}, k={}, result={:?} used={:?}", n, k, result, used);
+    if i >= k {
+        println!("answer={:?}", result);
+    } else {
+        for j in 0..n as usize {
+            if !used[j] {
+                used[j] = true;
+                result[i] = j;
+                solve(n, k, i + 1, result, used);
+                used[j] = false;
+            }
+        }
+    }
+}
+fn permutation(n: usize, k: usize) {
+    let mut result = vec![0 as usize; k];
+    let mut used = vec![false; n];
+//    result[1] = 100;
+//    used[1] = true;
+    println!("n={}, k={}, result={:?} used={:?}", n, k, result, used);
+    solve(n, k, 0, &mut result, &mut used);
+}
+
+fn main() {
+    permutation(3, 3);
+}
