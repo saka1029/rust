71294e57d5cdd0b397fbf10e5728b77411700b53
saka1029 <saka1029@gmail.com>
2025-09-20 22:20:57 +0900

    84

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,7 +3,12 @@ fn main() {
     let mut s = String::from(hello);
     s.push_str(", world!");
     println!("{}", s);    
-    let s1 = String::from("hello");
-    let s2 = s1;
-    println!("{}, world!", s2);
+    let mut s1 = String::from("hello");
+    let mut s2 = s1;
+    s2.push_str(", world!");
+    println!("{}", s2);
+    s1 = s2;    // 所有権もどす
+    println!("{}", s1);
+    let s3 = s1.clone();
+    println!("s1={}, s2={}", s1, s3);
 }
