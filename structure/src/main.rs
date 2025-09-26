f2a58c988ca8c537632760e9ef979ff32296b406
saka1029 <saka1029@gmail.com>
2025-09-26 17:29:55 +0900

    ..

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -22,6 +22,13 @@ fn main() {
         ..user1
     };
     print_user(&user2);
+    #[derive(Debug)]
+    struct Color(i32, i32, i32);
+    #[derive(Debug)]
+    struct Point(i32, i32, i32);
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    println!("black={:?}, origint={:?}", black, origin);
 }
 
 fn print_user(user: &User) {
