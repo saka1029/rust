1d322c5ee30587d370234e3e3ec7bd197257c4e8
saka1029 <saka1029@gmail.com>
2025-09-26 16:34:55 +0900

    107

---
+++ structure/src/main.rs
@@ -0,0 +1,39 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    user1.email = String::from("anothermail@example.com");
+    user1 = build_user(String::from("saka1029@gmail.com"),
+        String::from("saka1029"));
+    print_user(&user1);
+    let user2 = User {
+        email: String::from("user2@example.com"),
+        username: String::from("user2"),
+        ..user1
+    };
+    print_user(&user2);
+}
+
+fn print_user(user: &User) {
+    println!("User({}, {}, {}, {})",
+        user.username, user.email, user.active, user.sign_in_count);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
