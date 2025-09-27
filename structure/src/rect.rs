449f52d2f9cc9ee7f4af6efadc0e169fae5b5676
saka1029 <saka1029@gmail.com>
2025-09-27 11:50:32 +0900

    117

--- structure/src/rect.rs
+++ structure/src/rect.rs
@@ -8,14 +8,23 @@ fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
 
+// メソッド記法
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
 pub fn main_rect() {
     let rect1 = Rectangle { width:30, height: 50};
+    let rect2 = Rectangle { width:10, height: 40};
+    let rect3 = Rectangle { width:60, height: 45};
     println!("The area of {:?} is {}", &rect1, area(&rect1));
     println!("The area of {:?} is {}", &rect1, rect1.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
